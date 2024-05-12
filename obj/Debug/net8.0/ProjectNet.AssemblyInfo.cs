8efada8a012a074e24afe278cb74443d36ceb6cf
Cecilia Kembring <ceke2200@student.miun.se>
2024-05-12 15:32:35 +0200

    .git ignore

--- obj/Debug/net8.0/ProjectNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectNet.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb0c2dcea8fe20e92cf40986e80e528cd99c2ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
